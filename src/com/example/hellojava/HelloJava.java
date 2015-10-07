91b89f479c2f4c78fe9c29279646719efd034b6a
jsklenka <jp.sklenka@gmail.com>
2015-10-07 02:18:42 +0000

    Initial push

---
+++ src/com/example/hellojava/HelloJava.java
@@ -0,0 +1,12 @@
+package com.example.hellojava;
+
+/**
+ * Created by jsklenka on 10/7/2015.
+ */
+public class HelloJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Java!");
+    }
+}
